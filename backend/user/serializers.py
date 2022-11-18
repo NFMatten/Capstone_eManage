0f13fcc54efe15111932200942d99e47487d101d
Nikolaus Matten <nfmatten@gmail.com>
2022-11-18 15:51:26 -0700

    removed user app as it is unneeded

--- backend/user/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id','first_name','last_name','phone_number','address','dob','employee_role','salary','hire_date','emergency_contact','is_manager']
--- backend/user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.employee_list),
-    path('<int:pk>/', views.employee_detail)
-]
