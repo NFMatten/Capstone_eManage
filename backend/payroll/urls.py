48a197f77138f8cbae866eb5a82f1d1784aa60db
Nikolaus Matten <nfmatten@gmail.com>
2022-11-21 13:39:16 -0700

    feat: created new path to specific entry in payroll model

--- backend/payroll/urls.py
+++ backend/payroll/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.payroll_list)
+    path('', views.payroll_list),
+    path('<int:pk>/', views.payroll_detail)
 ]
