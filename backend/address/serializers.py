48d8d35f2ddeff361380348648f712f0a5099d0f
Nikolaus Matten <nfmatten@gmail.com>
2022-11-21 13:07:28 -0700

    feat: Created nested serializer to include username, first_name and last_name

--- backend/address/serializers.py
+++ backend/address/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from .models import Address
+from authentication.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name']
 
 class AddressSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False,read_only=True)
     class Meta:
         model = Address
-        fields = ['street_address', 'city','state','zip']
+        fields = ['id', 'user', 'street_address', 'city','state','zip', 'user_id']
+        depth = 1
+    user_id = serializers.IntegerField(write_only=True)
