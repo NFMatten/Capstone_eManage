4ca40d0c485a1b125095713f38426754de806b3e
Nikolaus Matten <nfmatten@gmail.com>
2022-11-21 13:28:37 -0700

    feat: Created model for Emergency Contact

--- backend/emergency_contact/models.py
+++ backend/emergency_contact/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
 class EmergencyContact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
     phone_number = models.CharField(max_length=12)
