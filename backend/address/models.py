bbd2d48aa64c5304cb0882b0caf51da6b1997919
Nikolaus Matten <nfmatten@gmail.com>
2022-11-21 13:06:46 -0700

    feat: Created model for Address

--- backend/address/models.py
+++ backend/address/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
 class Address(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
     street_address=models.CharField(max_length=255)
     city=models.CharField(max_length=255)
     state=models.CharField(max_length=2)
