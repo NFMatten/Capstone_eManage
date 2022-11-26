c0f06fe9e161f29c4fb9aaf764f3d08b6ccbf9a8
Nikolaus Matten <nfmatten@gmail.com>
2022-11-26 11:39:57 -0700

    style: Update styling to Login Page

--- frontend/src/pages/LoginPage/LoginPage.js
+++ frontend/src/pages/LoginPage/LoginPage.js
@@ -3,12 +3,13 @@ import AuthContext from "../../context/AuthContext";
 import useCustomForm from "../../hooks/useCustomForm";
 import "./LoginPage.css";
 import Stack from "@mui/material/Stack";
-import Container from "@mui/material/Container";
+import { Container, Box } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Checkbox from "@mui/material/Checkbox";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import Paper from "@mui/material/Paper";
 
 const LoginPage = () => {
   const { loginUser, isServerError } = useContext(AuthContext);
@@ -29,48 +30,51 @@ const LoginPage = () => {
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
   };
-
   return (
-    <Container maxWidth="xs">
-      <form className="form" onSubmit={handleSubmit}>
-        <Stack spacing={2}>
-          <h2>Sign in to eManage</h2>
-          <TextField
-            label="Username"
-            placeholder="Enter Username..."
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleInputChange}
-          />
+    <div>
+      <Box>
+        <form className="form" onSubmit={handleSubmit}>
+          <Paper elevation={3}>
+            <Stack spacing={2} sx={{ margin: "10px", alignItems: "center" }}>
+              <h2>Sign in to eManage</h2>
+              <TextField
+                label="Username"
+                placeholder="Enter Username..."
+                type="text"
+                name="username"
+                value={formData.username}
+                onChange={handleInputChange}
+              />
 
-          <TextField
-            label="Password"
-            placeholder="Enter Password..."
-            type={showPassword ? "test" : "password"}
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
+              <TextField
+                label="Password"
+                placeholder="Enter Password..."
+                type={showPassword ? "test" : "password"}
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
 
-          <FormGroup>
-            <FormControlLabel
-              control={<Checkbox onChange={toggleShowPassword} />}
-              label="Show Password"
-            />
-          </FormGroup>
+              <FormGroup>
+                <FormControlLabel
+                  control={<Checkbox onChange={toggleShowPassword} />}
+                  label="Show Password"
+                />
+              </FormGroup>
 
-          {isServerError ? (
-            <p className="error">Login failed, incorrect credentials!</p>
-          ) : null}
+              {isServerError ? (
+                <p className="error">Login failed, incorrect credentials!</p>
+              ) : null}
 
-          <Button href="/register">Click to register!</Button>
-          <Button variant="contained" type="submit">
-            Login!
-          </Button>
-        </Stack>
-      </form>
-    </Container>
+              <Button href="/register">Click to register</Button>
+              <Button variant="contained" type="submit">
+                Login!
+              </Button>
+            </Stack>
+          </Paper>
+        </form>
+      </Box>
+    </div>
   );
 };
 
