303cb257c27a5dd39c8c894aad1bb745b11905de
Nikolaus Matten <nfmatten@gmail.com>
2022-11-28 10:52:39 -0700

    feat: Created functional form to submit new announcement

--- frontend/src/components/AnnouncementForm/AnnouncementForm.jsx
+++ frontend/src/components/AnnouncementForm/AnnouncementForm.jsx
@@ -1,7 +1,41 @@
 import React, { useState } from "react";
+import { TextField, Button, Paper } from "@mui/material";
+import useAuth from "../../hooks/useAuth";
 
 const AnnouncementForm = (props) => {
-  return <></>;
+  const { addAnnouncement } = props;
+  const [value, setValue] = useState("");
+  const [user] = useAuth();
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addAnnouncement({ user_id: user.id, announcement: value });
+    setValue("");
+  };
+  return (
+    <div>
+      <Paper elevation={4} sx={{ my: 5 }}>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Announcement"
+          placeholder="Create a new Announcement"
+          multiline
+          maxRows={4}
+          value={value}
+          onChange={handleChange}
+          fullWidth
+          margin="normal"
+        />
+        <Button variant="contained" type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Paper>
+    </div>
+  );
 };
 
 export default AnnouncementForm;
