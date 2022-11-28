57ffa7f8e55201cd2fd88aac95ea84b821b47a7e
Nikolaus Matten <nfmatten@gmail.com>
2022-11-28 10:55:14 -0700

    feat: reversed mapping to show announcements in reverse order

--- frontend/src/components/AnnouncementList/AnnouncementList.jsx
+++ frontend/src/components/AnnouncementList/AnnouncementList.jsx
@@ -8,9 +8,12 @@ const AnnouncementList = (props) => {
     <Container>
       <Paper elevation={4} sx={{ my: 5 }}>
         <List>
-          {announcements.map((announcement) => (
-            <AnnouncementSingle {...announcement} key={announcement.id} />
-          ))}
+          {announcements
+            .slice(0)
+            .reverse()
+            .map((announcement) => (
+              <AnnouncementSingle {...announcement} key={announcement.id} />
+            ))}
         </List>
       </Paper>
     </Container>
