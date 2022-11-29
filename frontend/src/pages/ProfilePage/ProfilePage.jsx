1bed487e1cb81fb2ca232ce91d70bad5da91d506
Nikolaus Matten <nfmatten@gmail.com>
2022-11-29 15:06:40 -0700

    feat: Created async requests to GET, POST and PUT user's address and added props to ProfileMenu

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -8,9 +8,11 @@ import ProfileMenu from "../../components/ProfileMenu/ProfileMenu";
 const ProfilePage = (props) => {
   const [user, token] = useAuth();
   const [emergencyContact, setEmergencyContact] = useState("");
+  const [userAddress, setUserAddress] = useState("");
 
   useEffect(() => {
     getEmergencyContact();
+    getUserAddress();
   }, []);
 
   const getEmergencyContact = async () => {
@@ -22,7 +24,6 @@ const ProfilePage = (props) => {
         }
       );
       setEmergencyContact(response.data);
-      // console.log(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -58,12 +59,58 @@ const ProfilePage = (props) => {
     }
   };
 
+  const getUserAddress = async () => {
+    try {
+      const response = await axios.get(
+        `http://127.0.0.1:8000/address/${user.id}/`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      setUserAddress(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addUserAddress = async (addressObj) => {
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/address/",
+        addressObj,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      if (response.status === 201) getEmergencyContact();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateUserAddress = async (updatedAddress) => {
+    try {
+      const response = await axios.put(
+        `http://127.0.0.1:8000/address/${user.id}/`,
+        updatedAddress,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      if (response.status === 200) getUserAddress();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <ProfileMenu
         emergencyContact={emergencyContact}
         addEmergencyContact={addEmergencyContact}
         updateEmergencyContact={updateEmergencyContact}
+        userAddress={userAddress}
+        addUserAddress={addUserAddress}
+        updateUserAddress={updateUserAddress}
       />
     </div>
   );
