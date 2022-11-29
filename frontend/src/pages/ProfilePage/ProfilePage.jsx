651ae7a2b682c5499035662820ddbebfc4a9db4f
Nikolaus Matten <nfmatten@gmail.com>
2022-11-29 14:09:57 -0700

    feat: Created async function to update emergency contact with PUT request + refactor return to be ProfileMenu component

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -2,20 +2,27 @@ import React, { useState, useEffect } from "react";
 import EmergencyContactForm from "../../components/EmergencyContactForm/EmergencyContactForm";
 import useAuth from "../../hooks/useAuth";
 import axios from "axios";
+import DisplayEmergencyContact from "../../components/DisplayEmergencyContact/DisplayEmergencyContact";
+import ProfileMenu from "../../components/ProfileMenu/ProfileMenu";
 
 const ProfilePage = (props) => {
   const [user, token] = useAuth();
   const [emergencyContact, setEmergencyContact] = useState("");
 
+  useEffect(() => {
+    getEmergencyContact();
+  }, []);
+
   const getEmergencyContact = async () => {
     try {
       const response = await axios.get(
-        `http://127.0.0.1:8000/emergencyContact/${user.id}`,
+        `http://127.0.0.1:8000/emergencyContact/${user.id}/`,
         {
           headers: { Authorization: `Bearer ${token}` },
         }
       );
-      setEmergencyContact();
+      setEmergencyContact(response.data);
+      // console.log(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -35,9 +42,29 @@ const ProfilePage = (props) => {
       console.log(error);
     }
   };
+
+  const updateEmergencyContact = async (updatedInfo) => {
+    try {
+      const response = await axios.put(
+        `http://127.0.0.1:8000/emergencyContact/${user.id}/`,
+        updatedInfo,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      if (response.status === 200) getEmergencyContact();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <EmergencyContactForm addEmergencyContact={addEmergencyContact} />
+      <ProfileMenu
+        emergencyContact={emergencyContact}
+        addEmergencyContact={addEmergencyContact}
+        updateEmergencyContact={updateEmergencyContact}
+      />
     </div>
   );
 };
