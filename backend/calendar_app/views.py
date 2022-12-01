1d538dccb6ce9d105617affdd92579cf91e82d64
Nikolaus Matten <nfmatten@gmail.com>
2022-12-01 15:34:43 -0700

    feat: created CRUD functions

--- backend/calendar_app/views.py
+++ backend/calendar_app/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Calendar
+from .serializers import CalendarSerializer
 
-# Create your views here.
+@api_view(['GET','POST'])
+@permission_classes([IsAuthenticated])
+def list_of_events(request):
+    if request.method == 'GET':
+        events = Calendar.objects.all()
+        serializer = CalendarSerializer(events, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = CalendarSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET','PUT','DELETE'])
+@permission_classes([IsAuthenticated])
+def event_detail(request, pk):
+    event = get_object_or_404(Calendar, pk=pk)
+    if request.method == 'GET':
+        serializer = CalendarSerializer(event)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = CalendarSerializer(event, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
