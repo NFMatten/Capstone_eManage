81557030346c036ef8d8f85692865bce1a028fa2
Nikolaus Matten <nfmatten@gmail.com>
2022-12-02 15:21:33 -0700

    feat: Created Menu Items component to navigate to different pages based on if logged in user is manager or not

---
+++ frontend/src/components/MenuItems/MenuItems.jsx
@@ -0,0 +1,33 @@
+import { Paper, Link } from "@mui/material";
+import useAuth from "../../hooks/useAuth";
+
+const MenuItems = (props) => {
+  const [user, token] = useAuth();
+  return (
+    <div>
+      <Paper elevation={4} sx={{ marginBottom: "50px", alignItems: "center" }}>
+        <Link underline="hover" href="/calendar">
+          View Calendar
+        </Link>
+        {" | "}
+        <Link underline="hover" href="/payroll">
+          Payroll
+        </Link>
+        {" | "}
+        <Link underline="hover" href="/profile">
+          Update Personal Information
+        </Link>
+        {/* {" | "} */}
+        {user.is_manager ? (
+          <Link underline="hover" href="/employees">
+            | Employees
+          </Link>
+        ) : (
+          <></>
+        )}
+      </Paper>
+    </div>
+  );
+};
+
+export default MenuItems;
