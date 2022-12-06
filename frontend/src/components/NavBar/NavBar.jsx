0cf11abd07244f4f7000828f776cc9ffa23d0af0
Nikolaus Matten <nfmatten@gmail.com>
2022-12-06 11:31:14 -0700

    refactor: Added MenuItems component to NavBar + only displays component if user is logged in

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
+import MenuItems from "../MenuItems/MenuItems";
 import "./NavBar.css";
 
 const Navbar = () => {
@@ -9,40 +10,43 @@ const Navbar = () => {
   const navigate = useNavigate();
   return (
     <div className="navBar">
-      <ul className="ul">
-        <li className="brand li">
-          <Link
-            to="/"
-            title="Home"
-            style={{ textDecoration: "none", color: "white" }}
-          >
-            <b>eManage</b>
-          </Link>
-        </li>
-        <li className="li">
-          <img src="logo.png" alt="eManage logo" title="eManage"></img>
-        </li>
-        <li className="button-center li">
-          {user ? (
-            <>
-              <button onClick={() => navigate("/profile")}>Profile</button>
-              <button className="btn-space" onClick={logoutUser}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <button onClick={() => navigate("/login")}>Login</button>
-              <button
-                className="btn-space"
-                onClick={() => navigate("/register")}
-              >
-                Register
-              </button>
-            </>
-          )}
-        </li>
-      </ul>
+      <div>
+        <ul className="ul">
+          <li className="brand li">
+            <Link
+              to="/"
+              title="Home"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <b>eManage</b>
+            </Link>
+          </li>
+          <li className="li">
+            <img src="logo.png" alt="eManage logo" title="eManage"></img>
+          </li>
+          <li className="button-center li">
+            {user ? (
+              <>
+                <button onClick={() => navigate("/profile")}>Profile</button>
+                <button className="btn-space" onClick={logoutUser}>
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <button onClick={() => navigate("/login")}>Login</button>
+                <button
+                  className="btn-space"
+                  onClick={() => navigate("/register")}
+                >
+                  Register
+                </button>
+              </>
+            )}
+          </li>
+        </ul>
+      </div>
+      {user ? <MenuItems /> : <></>}
     </div>
   );
 };
