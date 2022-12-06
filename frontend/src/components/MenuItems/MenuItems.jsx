bde95288b35ec3cc68e14a534991a8de69f5177f
Nikolaus Matten <nfmatten@gmail.com>
2022-12-06 11:30:15 -0700

    style: Removed Paper component + changed text color to white

--- frontend/src/components/MenuItems/MenuItems.jsx
+++ frontend/src/components/MenuItems/MenuItems.jsx
@@ -5,26 +5,24 @@ const MenuItems = (props) => {
   const [user, token] = useAuth();
   return (
     <div>
-      <Paper elevation={4} sx={{ marginTop: "-25px", marginBottom: "50px" }}>
-        <Typography align="center">
-          <Link underline="hover" href="/calendar">
-            {"View Calendar |"}
+      <Typography align="center">
+        <Link underline="hover" color="common.white" href="/calendar">
+          {"View Calendar |"}
+        </Link>
+        <Link underline="hover" color="common.white" href="/payroll">
+          {" Payroll |"}
+        </Link>
+        <Link underline="hover" color="common.white" href="/profile">
+          {" Update Personal Information "}
+        </Link>
+        {user.is_manager ? (
+          <Link underline="hover" color="common.white" href="/employees">
+            {"| Employees"}
           </Link>
-          <Link underline="hover" href="/payroll">
-            {" Payroll |"}
-          </Link>
-          <Link underline="hover" href="/profile">
-            {" Update Personal Information "}
-          </Link>
-          {user.is_manager ? (
-            <Link underline="hover" href="/employees">
-              {"| Employees"}
-            </Link>
-          ) : (
-            <></>
-          )}
-        </Typography>
-      </Paper>
+        ) : (
+          <></>
+        )}
+      </Typography>
     </div>
   );
 };
