32f483ebe3ebfb627e8ffdf1c85c585c951ff022
Nikolaus Matten <nfmatten@gmail.com>
2022-12-05 21:17:49 -0700

    feat: Created API calls to fetch all Users and all Addresses + instaniated DisplayEmployees and DisplayEmployeeAddressess components

--- frontend/src/pages/EmployeesPage/EmployeesPage.jsx
+++ frontend/src/pages/EmployeesPage/EmployeesPage.jsx
@@ -1,5 +1,57 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import DisplayEmployeeAddresses from "../../components/DisplayEmployeeAddresses/DisplayEmployeeAddresses";
+import DisplayEmployees from "../../components/DisplayEmployees/DisplayEmployees";
+import useAuth from "../../hooks/useAuth";
+import Grid from "@mui/material/Grid";
+
 const EmployeesPage = (props) => {
-  return <></>;
+  const [allUsers, setAllUsers] = useState([]);
+  const [addresses, setAddresses] = useState([]);
+  const [user, token] = useAuth();
+
+  useEffect(() => {
+    fetchUsers();
+    fetchAddresses();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8000/api/auth/employees/",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      setAllUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchAddresses = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/address/", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setAddresses(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <DisplayEmployees allUsers={allUsers} />
+        </Grid>
+        <Grid item xs={12}>
+          <DisplayEmployeeAddresses addresses={addresses} />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default EmployeesPage;
