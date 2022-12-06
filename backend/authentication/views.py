9e9065a857efdbcd14c95d5214260674bcac8b8f
Nikolaus Matten <nfmatten@gmail.com>
2022-12-05 21:15:23 -0700

    feat: Created CRUD functions for users/employees

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -3,6 +3,13 @@ from .serializers import MyTokenObtainPairSerializer, RegistrationSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
 User = get_user_model()
 
 
@@ -14,3 +21,33 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def getUsers(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = RegistrationSerializer(users, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET','PUT','DELETE'])
+@permission_classes(IsAuthenticated)
+def specificUser(request, user_id):
+    requestedUser = get_object_or_404(User, user__id=user_id)
+    if request.method == 'GET':
+        requestedUser = User.objects.filter(user__id=user_id)
+        serializer = RegistrationSerializer(requestedUser, many=True)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = RegistrationSerializer(requestedUser, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        requestedUser.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
