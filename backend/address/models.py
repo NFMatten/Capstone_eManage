a6df488ac82b49fe031029a482a025e26154352c
Nikolaus Matten <nfmatten@gmail.com>
2022-12-06 10:29:49 -0700

    refactor: changed user from ForiegnKey to OneToOneField

--- backend/address/models.py
+++ backend/address/models.py
@@ -3,7 +3,7 @@ from authentication.models import User
 
 # Create your models here.
 class Address(models.Model):
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
     street_address=models.CharField(max_length=255)
     city=models.CharField(max_length=255)
     state=models.CharField(max_length=2)
