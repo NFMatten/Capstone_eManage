dcac272756d2251e8737d96c9885ddd7f88810b8
Nikolaus Matten <nfmatten@gmail.com>
2022-12-06 08:22:31 -0700

    style: Updated styling for MenuItems component

--- frontend/src/components/MenuItems/MenuItems.jsx
+++ frontend/src/components/MenuItems/MenuItems.jsx
@@ -1,27 +1,29 @@
-import { Paper, Link } from "@mui/material";
+import { Paper, Link, Typography } from "@mui/material";
 import useAuth from "../../hooks/useAuth";
 
 const MenuItems = (props) => {
   const [user, token] = useAuth();
   return (
     <div>
-      <Paper elevation={4} sx={{ marginBottom: "50px", alignItems: "center" }}>
-        <Link underline="hover" href="/calendar">
-          {"View Calendar |"}
-        </Link>
-        <Link underline="hover" href="/payroll">
-          {" Payroll |"}
-        </Link>
-        <Link underline="hover" href="/profile">
-          {" Update Personal Information "}
-        </Link>
-        {user.is_manager ? (
-          <Link underline="hover" href="/employees">
-            {"| Employees"}
+      <Paper elevation={4} sx={{ marginTop: "-25px", marginBottom: "50px" }}>
+        <Typography align="center">
+          <Link underline="hover" href="/calendar">
+            {"View Calendar |"}
           </Link>
-        ) : (
-          <></>
-        )}
+          <Link underline="hover" href="/payroll">
+            {" Payroll |"}
+          </Link>
+          <Link underline="hover" href="/profile">
+            {" Update Personal Information "}
+          </Link>
+          {user.is_manager ? (
+            <Link underline="hover" href="/employees">
+              {"| Employees"}
+            </Link>
+          ) : (
+            <></>
+          )}
+        </Typography>
       </Paper>
     </div>
   );
