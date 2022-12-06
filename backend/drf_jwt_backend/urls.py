b26d367a7cbb34d89e1304b272b00a3e196010d3
Nikolaus Matten <nfmatten@gmail.com>
2022-12-05 21:16:03 -0700

    feat: Created path to employees

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
+    path('api/auth/employees', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
     path('announcements/', include('announcements.urls')),
     path('payroll/', include('payroll.urls')),
