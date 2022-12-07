17fb875b3c6973e0fc0e3ddf9cf175497382a7d9
Nikolaus Matten <nfmatten@gmail.com>
2022-12-06 18:11:39 -0700

    feat: Instantiated DisplayEmergencyContacts component + create API call to fetch all emergency contacts

--- frontend/src/pages/EmployeesPage/EmployeesPage.jsx
+++ frontend/src/pages/EmployeesPage/EmployeesPage.jsx
@@ -3,16 +3,19 @@ import React, { useState, useEffect } from "react";
 import DisplayEmployeeAddresses from "../../components/DisplayEmployeeAddresses/DisplayEmployeeAddresses";
 import DisplayEmployees from "../../components/DisplayEmployees/DisplayEmployees";
 import useAuth from "../../hooks/useAuth";
-import Grid from "@mui/material/Grid";
+import { Grid, Container } from "@mui/material/";
+import DisplayEmergencyContacts from "../../components/DisplayEmergencyContacts/DisplayEmergencyContacts";
 
 const EmployeesPage = (props) => {
   const [allUsers, setAllUsers] = useState([]);
   const [addresses, setAddresses] = useState([]);
+  const [emergContacts, setEmergContacts] = useState([]);
   const [user, token] = useAuth();
 
   useEffect(() => {
     fetchUsers();
     fetchAddresses();
+    fetchEmergencyContacts();
   }, []);
 
   const fetchUsers = async () => {
@@ -40,24 +43,44 @@ const EmployeesPage = (props) => {
     }
   };
 
+  const fetchEmergencyContacts = async () => {
+    try {
+      let response = await axios.get(
+        "http://localhost:8000/emergencyContact/",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      setEmergContacts(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>
+    <Container maxWidth="xl">
       <p>ToDo: Emergency Contact Table, Filter tables</p>
       Styling: Sidebar? Button to display Addresses and Emergency Contacts with
       default table not shown
       <br />
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
+      <Grid container spacing={2} sx={{ justifyContent: "center" }}>
+        <Grid item xs={9}>
           <DisplayEmployees allUsers={allUsers} fetchUsers={fetchUsers} />
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={9}>
           <DisplayEmployeeAddresses
             addresses={addresses}
             fetchAddresses={fetchAddresses}
           />
         </Grid>
+        <Grid item xs={9}>
+          <DisplayEmergencyContacts
+            emergContacts={emergContacts}
+            fetchEmergencyContacts={fetchEmergencyContacts}
+          />
+        </Grid>
       </Grid>
-    </div>
+    </Container>
   );
 };
 
