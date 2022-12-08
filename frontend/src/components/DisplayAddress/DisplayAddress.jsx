6d44e320d87e727826c0b9c75b9556e8dc42c294
Nikolaus Matten <nfmatten@gmail.com>
2022-12-08 09:05:32 -0700

    style: Updated styling for displayed layout and color scheme

--- frontend/src/components/DisplayAddress/DisplayAddress.jsx
+++ frontend/src/components/DisplayAddress/DisplayAddress.jsx
@@ -1,29 +1,50 @@
 import React, { useState } from "react";
-import { Paper, Typography, Container, Button } from "@mui/material";
+import { Paper, Typography, Container, Grid, Tooltip } from "@mui/material";
 import AddressForm from "../AddressForm/AddressForm";
-import useAuth from "../../hooks/useAuth";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
 
 const DisplayAddress = (props) => {
   const [showForm, setShowForm] = useState(false);
-  const { street_address, city, state, zip, updateUserAddress } = props;
-  const [user, token] = useAuth();
+  const { street_address, city, state, zip, updateUserAddress, user } = props;
 
   const toggleForm = () => setShowForm(!showForm);
   return (
     <Container>
-      <Paper elevation={4} sx={{ my: 3 }}>
-        <Typography variant="h6">{user.first_name}'s Address</Typography>
-        <Typography>
-          {street_address}
-          <br />
-          {city}, {state}
-          <br />
-          {zip}
+      <Paper elevation={4} sx={{ my: 3, minHeight: 200 }}>
+        <Typography
+          variant="h5"
+          color="white"
+          sx={{
+            textAlign: "center",
+            py: "10px",
+            backgroundColor: "#ffc163",
+            boxShadow: 3,
+          }}
+        >
+          {user.first_name}'s Address
         </Typography>
-        <Button variant="contained" onClick={toggleForm}>
-          Edit Address
-        </Button>
-        {showForm && <AddressForm callBack={updateUserAddress} />}
+        <Grid container justifyContent="center" sx={{ marginTop: 2 }}>
+          <Grid item xs={6.5}>
+            <Typography variant="h6" sx={{ textAlign: "right" }}>
+              {street_address}
+              <br />
+              {city}, {state}
+              <br />
+              {zip}
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Grid container sx={{ paddingTop: 3 }}>
+              <Tooltip title="Edit your address">
+                <IconButton variant="contained" onClick={toggleForm}>
+                  <EditIcon style={{ color: "orange" }} />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+          </Grid>
+          {showForm && <AddressForm callBack={updateUserAddress} />}
+        </Grid>
       </Paper>
     </Container>
   );
--- frontend/src/components/DisplayEmergencyContact/DisplayEmergencyContact.jsx
+++ frontend/src/components/DisplayEmergencyContact/DisplayEmergencyContact.jsx
@@ -1,25 +1,54 @@
 import React, { useState } from "react";
-import { Paper, Typography, Container, Button } from "@mui/material";
+import { Paper, Typography, Container, Grid, Tooltip } from "@mui/material";
 import EmergencyContactForm from "../EmergencyContactForm/EmergencyContactForm";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
 
 const DisplayEmergencyContact = (props) => {
   const [showForm, setShowForm] = useState(false);
-  const { first_name, last_name, phone_number, updateEmergencyContact } = props;
+  const { first_name, last_name, phone_number, updateEmergencyContact, user } =
+    props;
 
   const toggleForm = () => setShowForm(!showForm);
 
   return (
     <Container>
-      <Paper elevation={4} sx={{ my: 3 }}>
-        <Typography variant="h6">Emergency Contact</Typography>
-        <Typography sx={{ marginLeft: "20px" }}>
-          {first_name} {last_name}
+      <Paper elevation={4} sx={{ my: 3, minHeight: 200 }}>
+        <Typography
+          variant="h5"
+          color="white"
+          sx={{
+            textAlign: "center",
+            py: "10px",
+            backgroundColor: "#ffc163",
+            boxShadow: 3,
+          }}
+        >
+          {user.first_name}'s Emergency Contact
         </Typography>
-        <Typography sx={{ marginLeft: "20px" }}>{phone_number}</Typography>
-        <Button variant="contained" onClick={toggleForm}>
-          Edit Emergency Contact
-        </Button>
-        {showForm && <EmergencyContactForm callBack={updateEmergencyContact} />}
+        <Grid container sx={{ marginTop: 2 }}>
+          <Grid item xs={7}>
+            <Typography variant="h6" sx={{ textAlign: "right" }}>
+              {first_name} {last_name}
+            </Typography>
+            <Typography variant="h6" sx={{ textAlign: "right" }}>
+              {phone_number}
+            </Typography>
+          </Grid>
+          <Grid item xs={5}>
+            <Grid container sx={{ paddingTop: 3 }}>
+              <Tooltip title="Edit your Emergency Contact">
+                <IconButton variant="contained" onClick={toggleForm}>
+                  <EditIcon style={{ color: "orange" }} />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+          </Grid>
+
+          {showForm && (
+            <EmergencyContactForm callBack={updateEmergencyContact} />
+          )}
+        </Grid>
       </Paper>
     </Container>
   );
