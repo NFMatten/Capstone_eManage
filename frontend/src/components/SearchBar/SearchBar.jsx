59a0c99260ae2ad1efbeffe7495587a8acbb4595
Nikolaus Matten <nfmatten@gmail.com>
2022-12-08 07:56:44 -0700

    feat: updated onChange to include filtering addresses and emergency contacts

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -6,7 +6,11 @@ const SearchBar = (props) => {
       <input
         type="text"
         placeholder="Search"
-        onChange={(e) => props.filterEmployees(e)}
+        onChange={(e) => {
+          props.filterEmployees(e);
+          props.filterAddresses(e);
+          props.filterEmergencyContacts(e);
+        }}
       />
     </div>
   );
