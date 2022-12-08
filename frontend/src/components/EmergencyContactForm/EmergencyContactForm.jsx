c1a972d366db2b202a73e565fd97489b1a5be593
Nikolaus Matten <nfmatten@gmail.com>
2022-12-08 09:04:28 -0700

    style: Updated styling for displayed layout, color scheme and edit icon

--- frontend/src/components/EmergencyContactForm/EmergencyContactForm.jsx
+++ frontend/src/components/EmergencyContactForm/EmergencyContactForm.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { Paper, TextField, Button, Typography, Container } from "@mui/material";
+import {
+  Paper,
+  TextField,
+  Button,
+  Typography,
+  Container,
+  Grid,
+} from "@mui/material";
 import useAuth from "../../hooks/useAuth";
 
 const EmergencyContactForm = (props) => {
@@ -21,37 +28,59 @@ const EmergencyContactForm = (props) => {
 
   return (
     <Container>
-      <Paper elevation={4}>
-        <Typography>Emergency Contact Information</Typography>
-        <TextField
-          id="first_name"
-          label="First Name"
-          placeholder="Enter First Name"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-        <TextField
-          id="last_name"
-          label="Last Name"
-          placeholder="Enter Last Name"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-        <TextField
-          id="phone_number"
-          label="Phone Number"
-          placeholder="Enter Phone Number"
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-        />
-        <Button
-          variant="contained"
-          type="submit"
-          onClick={handleSubmit}
-          sx={{ backgroundColor: "#ffc163" }}
-        >
-          Submit
-        </Button>
+      <Paper elevation={4} sx={{ backgroundColor: "#fff0d8", my: 2 }}>
+        <Typography align="center">
+          <b>Emergency Contact Information</b>
+        </Typography>
+        <Grid container>
+          <Grid item xs={6}>
+            <TextField
+              id="first_name"
+              label="First Name"
+              placeholder="Enter First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              sx={{ my: 1, mx: 1, backgroundColor: "white" }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="last_name"
+              label="Last Name"
+              placeholder="Enter Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              sx={{ my: 1, mx: 1, backgroundColor: "white" }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="phone_number"
+              label="Phone Number"
+              placeholder="Enter Phone Number"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              sx={{ marginBottom: 1, mx: 1, backgroundColor: "white" }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <Grid container justifyContent="center">
+              <Button
+                variant="contained"
+                type="submit"
+                onClick={handleSubmit}
+                sx={{
+                  backgroundColor: "#ffc163",
+                  "&:hover": { backgroundColor: "#ffb74a" },
+                  my: 1,
+                  minWidth: "200px",
+                }}
+              >
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
       </Paper>
     </Container>
   );
