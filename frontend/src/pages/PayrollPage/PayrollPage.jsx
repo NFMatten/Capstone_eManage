0892de4548976a6a2fbdd1f599b74fe07a332a7e
Nikolaus Matten <nfmatten@gmail.com>
2022-12-08 16:26:41 -0700

    feat: created page that makes API calls and displays components based on user 'is_manager'. As a manager, the user can create a payroll using a table. As an employee, the user can input date to estimate their pay

--- frontend/src/pages/PayrollPage/PayrollPage.jsx
+++ frontend/src/pages/PayrollPage/PayrollPage.jsx
@@ -1,5 +1,72 @@
+import EmployeePayroll from "../../components/Payroll/EmployeePayroll";
+import ManagerPayroll from "../../components/Payroll/ManagerPayroll";
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
 const PayrollPage = (props) => {
-  return <></>;
+  const [user, token] = useAuth();
+  const [payroll, setPayroll] = useState([]);
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    fetchPayroll();
+    fetchEmployees();
+  }, []);
+
+  const fetchPayroll = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/payroll/", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setPayroll(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchEmployees = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8000/api/auth/employees/",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      setEmployees(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addToPayroll = async (newEntry) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/payroll/",
+        newEntry,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      if (response.status === 201) fetchPayroll();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      {user.is_manager ? (
+        <ManagerPayroll
+          employees={employees}
+          fetchPayroll={fetchPayroll}
+          token={token}
+        />
+      ) : (
+        <EmployeePayroll />
+      )}
+    </div>
+  );
 };
 
 export default PayrollPage;
