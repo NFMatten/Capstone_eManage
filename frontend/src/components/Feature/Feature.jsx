0d0fbc938d9941e484f12c68833e98b7583d3180
Nikolaus Matten <nfmatten@gmail.com>
2022-12-09 13:03:41 -0700

    style: updated styling to the feature component to match the theme of the webpage

--- frontend/src/components/Feature/Feature.jsx
+++ frontend/src/components/Feature/Feature.jsx
@@ -4,17 +4,30 @@ const Features = (props) => {
   const { feature, description, image } = props;
   return (
     <Grid item xs={6} md={5}>
-      <Paper elevation={4} sx={{ justifyContent: "center", padding: 5 }}>
+      <Paper elevation={4} sx={{ justifyContent: "center" }}>
         <Typography
           variant="h5"
+          color="white"
           sx={{
-            textDecoration: "underline",
-            textDecorationColor: "orange",
+            textAlign: "center",
+            py: "10px",
+            mb: 3,
+            backgroundColor: "#ffc163",
+            boxShadow: 3,
           }}
         >
           {`${feature}`}
         </Typography>
-        <Typography variant="subtitle2">{`${description}`}</Typography>
+        <Typography
+          variant="body1"
+          textAlign="center"
+          sx={{
+            fontWeight: 600,
+            textDecoration: "underline",
+            textDecorationColor: "orange",
+            textDecorationThickness: 2,
+          }}
+        >{`${description}`}</Typography>
         <br />
         <CardMedia
           component="img"
