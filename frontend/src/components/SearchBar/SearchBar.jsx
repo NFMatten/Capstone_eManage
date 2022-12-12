411cf54b21236f43f757297b2c9c0fc24672352b
Nikolaus Matten <nfmatten@gmail.com>
2022-12-12 10:00:49 -0700

    feat: Added instructions for filtering tables + updated styling to the component

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,50 @@
 import React from "react";
+import { Paper, Typography, TextField, Grid } from "@mui/material";
 
 const SearchBar = (props) => {
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Search"
-        onChange={(e) => {
-          props.filterEmployees(e);
-          props.filterAddresses(e);
-          props.filterEmergencyContacts(e);
-        }}
-      />
+      <Paper>
+        <Typography
+          variant="h5"
+          color="white"
+          sx={{
+            textAlign: "center",
+            py: "10px",
+            backgroundColor: "#ffc163",
+            boxShadow: 3,
+          }}
+        >
+          Filter Tables
+        </Typography>
+        <Grid container sx={{ justifyContent: "center" }}>
+          <Grid item xs={12} sx={{ pt: 1.5 }}>
+            <Typography variant="body2" textAlign="center">
+              To filter all tables, please enter an Employee Name
+            </Typography>
+            <Typography variant="body2" textAlign="center">
+              Or, filter the Employees Table by Name, Email, Phone Number,
+              Salary, Hire Date or Job Title
+            </Typography>
+          </Grid>
+          <Grid item xs={9}>
+            <TextField
+              type="text"
+              placeholder="Search"
+              label="Filter Tables"
+              variant="filled"
+              size="small"
+              fullWidth
+              onChange={(e) => {
+                props.filterEmployees(e);
+                props.filterAddresses(e);
+                props.filterEmergencyContacts(e);
+              }}
+              sx={{ my: 3 }}
+            />
+          </Grid>
+        </Grid>
+      </Paper>
     </div>
   );
 };
