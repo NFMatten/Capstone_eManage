21964ee0b8bf2635ba5c11f296a61e61030a4d0d
NikFMatten <nfmatten@gmail.com>
2023-01-02 11:48:21 -0700

    refactor: Updated Navbar to completely new NavBar which includes drawers

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,57 +1,118 @@
-import React from "react";
 import { useContext } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
-import MenuItems from "../MenuItems/MenuItems";
+import CustomDrawer from "./CustomDrawer";
 import "./NavBar.css";
+
+import * as React from "react";
+import Link from "@mui/material/Link";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 
 const Navbar = () => {
   const { logoutUser, user } = useContext(AuthContext);
   const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const profileClick = () => {
+    setAnchorEl(null);
+    navigate("/profile");
+  };
+
+  const logoutClick = () => {
+    setAnchorEl(null);
+    logoutUser();
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={profileClick}>Profile</MenuItem>
+      <MenuItem onClick={logoutClick}>Logout</MenuItem>
+    </Menu>
+  );
+
   return (
-    <div className="navBar">
-      <div>
-        <ul className="ul">
-          <li className="brand li">
-            <Link
-              to="/"
-              title="Home"
-              style={{ textDecoration: "none", color: "white" }}
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: "#ffc163", mb: 5 }}>
+        <Toolbar>
+          {user ? <CustomDrawer /> : <></>}
+
+          <Link href="/" underline="none">
+            <Typography
+              variant="h4"
+              noWrap
+              sx={{
+                display: { xs: "none", sm: "block" },
+                color: "#ffffff",
+              }}
             >
-              <b>eManage</b>
-            </Link>
-          </li>
-          <li className="li">
-            <Link to="/">
-              <img src="logo.png" alt="eManage logo" title="eManage"></img>
-            </Link>
-          </li>
-          <li className="button-center li">
+              eManage
+            </Typography>
+          </Link>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
             {user ? (
-              <>
-                <button onClick={() => navigate("/profile")}>Profile</button>
-                <button className="btn-space" onClick={logoutUser}>
-                  Logout
-                </button>
-              </>
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle fontSize="large" />
+              </IconButton>
             ) : (
               <>
-                <button onClick={() => navigate("/login")} data-test="login">
-                  Login
+                <button className="button" onClick={() => navigate("/login")}>
+                  Log In
                 </button>
                 <button
-                  className="btn-space"
+                  className="button btn-space"
                   onClick={() => navigate("/register")}
                 >
                   Register
                 </button>
               </>
             )}
-          </li>
-        </ul>
-      </div>
-      {user ? <MenuItems /> : <></>}
-    </div>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMenu}
+    </Box>
   );
 };
 
